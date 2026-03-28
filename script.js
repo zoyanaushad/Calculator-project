5a18a35c6f27458be0def61b9e527092e2a24fd1
Zoya Naushad <zoyanaushad.3@gmail.com>
2026-03-29 02:56:37 +0530

    Updated code

--- script.js
+++ script.js
@@ -5,6 +5,23 @@ let display = document.getElementById("display");
 const operators = ["+", "-", "/", "*", "%"];
 
 function press(value) {
+    let lastChar = display.value.slice(-1);
+
+  // get last number
+  let parts = display.value.split(/[\+\-\*\/%]/);
+  let lastNumber = parts[parts.length - 1];
+
+  // block multiple leading zeros
+  if (lastNumber === "0" && value === "0") {
+    return;
+  }
+
+  // replace 0 with number (e.g. 0 → 5)
+  if (lastNumber === "0" && value !== "0" && value !== ".") {
+    display.value = val.slice(0, -1) + value;
+    return;
+  }
+
     display.value += value;
 }
 
@@ -13,7 +30,12 @@ function clearDisplay() {
 }
 
 function calculateResult() {
-    display.value = eval(display.value);
+    try {
+  display.value = eval(display.value);
+}
+ catch {
+  display.value = "Error";
+}
 }
 
 function deleteLast() {
@@ -21,15 +43,56 @@ function deleteLast() {
 }
 
 function addOperator(op) {
-    if (display.value === "" && op !== "-") return;
+  let lastChar = display.value.slice(-1);
 
-    let lastChar = display.value.slice(-1);
-
-    if (operators.includes(lastChar)){
-        display.value = display.value.slice(0. -1) - op;
-        return;
+  // allow only "-" at start
+  if (display.value === "") {
+    if (op === "-") {
+      display.value += op;
     }
-    display.value += op
+    return;
+  }
+
+  // block if last is operator
+  if (operators.includes(lastChar)) {
+
+    if (op === "-") {
+      display.value += "-";
+    }
+
+    return;
+  }
+
+  // add operator
+  display.value += op;
+}
+// 🔹 Decimal
+function addDecimal() {
+  let val = display.value;
+  let lastChar = val.slice(-1);
+
+  // start with 0.
+  if (val === "") {
+    display.value = "0.";
+    return;
+  }
+
+  // after operator → add 0.
+  if (operators.includes(lastChar)) {
+    display.value += "0.";
+    return;
+  }
+
+  // get last number
+  let parts = val.split(/[\+\-\*\/%]/);
+  let lastNumber = parts[parts.length - 1];
+
+  // block multiple decimals
+  if (lastNumber.includes(".")) {
+    return;
+  }
+
+  display.value += ".";
 }
 
 /*document.addEventListener("keydown", function(event) {
